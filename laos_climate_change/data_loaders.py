0fc42834ffc445bdd90d1381423148c41eb57ece
Camilo Saldarriaga <jcamsalda@gmail.com>
2024-05-20 20:33:57 +0200

    including Jesse's change requirements

--- laos_climate_change/data_loaders.py
+++ laos_climate_change/data_loaders.py
@@ -1,27 +1,29 @@
-# Verify world shapefile
+import os
 from os.path import exists
 from urllib.request import urlretrieve
 
-path_to_file = "C:/Users/camil/Dropbox/Servicios/OECD-GGGI/laos-climate-change/laos_climate_change/wb_countries_admin0_10m.zip"
-url = "https://datacatalogfiles.worldbank.org/ddh-published/0038272/DR0046659/wb_countries_admin0_10m.zip?versionId=2024-05-14T14:58:01.5696428Z"
-filename = "wb_countries_admin0_10m.zip"
+world_url = "https://datacatalogfiles.worldbank.org/ddh-published/0038272/DR0046659/wb_countries_admin0_10m.zip?versionId=2024-05-14T14:58:01.5696428Z"
+world_filename = "wb_countries_admin0_10m.zip"
+laos_url = "https://data.humdata.org/dataset/9eb6aff1-9e3f-43d3-99a6-f415fe4b4dff/resource/1d6edf99-5303-4b31-8909-dd70cda78443/download/lao_adm_ngd_20191112_shp.zip"
+laos_filename = "lao_adm_ngd_20191112_shp.zip"
 
 
-def download_shape_files():
+def download_shapefile(
+    which, output_path=".../laos-climate-change/laos_climate_change"
+):
+    if which == "Laos":
+        url = laos_url
+        filename = laos_filename
+        path_to_file = output_path + "/" + filename
+    elif which == "world":
+        url = world_url
+        filename = world_filename
+        path_to_file = output_path + "/" + filename
+    else:
+        raise NotImplementedError()
+
+    if not exists(output_path):
+        os.makedirs(output_path)
+
     if not exists(path_to_file):
         urlretrieve(url, filename)
-    else:
-        filename
-
-
-# Verify Laos shapefile
-path_to_file2 = "lao_adm_ngd_20191112_shp.zip"
-url2 = "https://data.humdata.org/dataset/9eb6aff1-9e3f-43d3-99a6-f415fe4b4dff/resource/1d6edf99-5303-4b31-8909-dd70cda78443/download/lao_adm_ngd_20191112_shp.zip"
-filename2 = "lao_adm_ngd_20191112_shp.zip"
-
-
-def download_shape_files2():
-    if not exists(path_to_file2):
-        urlretrieve(url2, filename2)
-    else:
-        filename2
