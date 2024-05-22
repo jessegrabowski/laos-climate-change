052dcfe99068c293a32e522d408e59880c1b20fe
Camilo Saldarriaga <jcamsalda@gmail.com>
2024-05-22 15:24:36 -0600

    applying required changes

---
+++ laos_gggi/constants.py
@@ -0,0 +1,28 @@
+prob_cols = [  # noqa
+        "Drought",
+        "Earthquake",
+        "Extreme temperature",
+        "Flood",
+        "Storm",
+        "Wildfire",
+        "Volcanic activity",
+        "Mass Movement (Dry)",
+        "Mass Movement (Wet)",
+    ]  # noqa
+
+    intensity_cols = [
+        "Country",
+        "Start Year",
+        "Total Deaths",
+        "No. Injured",
+        "No. Affected",
+        "No. Homeless",
+        "Total Affected",
+        "Reconstruction Costs ('000 US$)",
+        "Reconstruction Costs, Adjusted ('000 US$)",
+        "Insured Damage ('000 US$)",
+        "Insured Damage, Adjusted ('000 US$)",
+        "Total Damage ('000 US$)",
+        "Total Damage, Adjusted ('000 US$)",
+        "CPI",
+    ]
