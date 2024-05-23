b885565622eace851ce9d85d172965d4f0aa7931
CamSalda <131469693+CamSalda@users.noreply.github.com>
2024-05-23 09:38:39 -0600

    Emdat processing script (#15)
    * creating fileshape importers
    
    * shapefile
    
    * including Jesse's change requirements
    
    * adding specific files to the .gitignore file
    
    * adding python scripts to process mdata
    
    * modifiying emdat processing script
    
    * adding #noqa to emdat processing script
    
    * applying required changes
    
    * adding changes required by Jesse
    
    * set of changes may 23
    
    * final set of changes

---
+++ laos_gggi/const_vars.py
@@ -0,0 +1,42 @@
+PROB_COLS = [  # noqa
+    "Drought",
+    "Earthquake",
+    "Extreme temperature",
+    "Flood",
+    "Storm",
+    "Wildfire",
+    "Volcanic activity",
+    "Mass Movement (Dry)",
+    "Mass Movement (Wet)",
+]
+
+DISASTERS_FOUND = [  # noqa
+    "Drought",
+    "Earthquake",
+    "Extreme temperature",
+    "Flood",
+    "Storm",
+    "Volcanic activity",
+    "Wildfire",
+]
+
+INTENSITY_COLS = [  # noqa
+    "Country",
+    "Start Year",
+    "Total Deaths",
+    "No. Injured",
+    "No. Affected",
+    "No. Homeless",
+    "Total Affected",
+    "Reconstruction Costs ('000 US$)",
+    "Reconstruction Costs, Adjusted ('000 US$)",
+    "Insured Damage ('000 US$)",
+    "Insured Damage, Adjusted ('000 US$)",
+    "Total Damage ('000 US$)",
+    "Total Damage, Adjusted ('000 US$)",
+    "CPI",
+]
+WORLD_URL = "https://datacatalogfiles.worldbank.org/ddh-published/0038272/DR0046659/wb_countries_admin0_10m.zip?versionId=2024-05-14T14:58:01.5696428Z"  # noqa
+WORLD_FILENAME = "wb_countries_admin0_10m.zip"  # noqa
+LAOS_URL = "https://data.humdata.org/dataset/9eb6aff1-9e3f-43d3-99a6-f415fe4b4dff/resource/1d6edf99-5303-4b31-8909-dd70cda78443/download/lao_adm_ngd_20191112_shp.zip"  # noqa
+LAOS_FILENAME = "lao_adm_ngd_20191112_shp.zip"  # noqa
---
+++ laos_gggi/shapefiles_data_loader.py
@@ -0,0 +1,24 @@
+import os
+from os.path import exists
+from urllib.request import urlretrieve
+from const_vars import WORLD_URL, WORLD_FILENAME, LAOS_URL, LAOS_FILENAME
+
+
+def download_shapefile(which, output_path="../data/shapefiles"):
+    if which == "Laos":
+        url = LAOS_URL
+        filename = LAOS_FILENAME
+        path_to_file = output_path + "/" + filename
+    elif which == "world":
+        url = WORLD_URL
+        filename = WORLD_FILENAME
+        path_to_file = output_path + "/" + filename
+    else:
+        raise NotImplementedError()
+
+    if not exists(output_path):
+        os.makedirs(output_path)
+        urlretrieve(url, path_to_file)
+
+    if not exists(path_to_file):
+        urlretrieve(url, path_to_file)
