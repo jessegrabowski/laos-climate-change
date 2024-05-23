f39c0d2a6602675449dc4104b67ccb664f9e8bee
CamSalda <131469693+CamSalda@users.noreply.github.com>
2024-05-23 09:38:39 -0600

    Emdat processing script (#15)
    * creating fileshape importers
    
    * shapefile
    
    * including Jesse's change requirements
    
    * adding specific files to the .gitignore file
    
    * adding python scripts to process mdata
    
    * modifiying emdat processing script
    
    * adding #noqa to emdat processing script
    
    * applying required changes
    
    * adding changes required by Jesse
    
    * set of changes may 23
    
    * final set of changes

---
+++ laos_gggi/emdat_processing.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import os
+from os.path import exists
+from const_vars import INTENSITY_COLS, DISASTERS_FOUND
+
+
+def process_emdat(data_path="../data"):
+    if not exists(data_path):
+        os.makedirs(data_path)
+
+    if not exists(data_path + "/emdat.xlsx"):
+        raise NotImplementedError(
+            "No EM-DAT data was found at `/data/emdat.xlsx`. Please make an account at <url>, download the database, and place it in `/data/emdat.xlsx`"
+        )
+
+    df = pd.read_excel(data_path + "/emdat.xlsx", sheet_name="EM-DAT Data")
+
+    df2 = df.copy()[INTENSITY_COLS]
+
+    df = (
+        df.query("`Disaster Type` in @PROB_COLS")
+        .groupby(["Disaster Type", "Country", "Start Year"])
+        .size()
+        .unstack("Disaster Type")
+        .fillna(0)
+        .reset_index()
+    ).copy()
+
+    df[DISASTERS_FOUND] = df[DISASTERS_FOUND].astype(int).copy()
+
+    df_prob = df.copy().set_index(["Country", "Start Year"]).sort_index()
+    df_inten = df2.copy().set_index(["Country", "Start Year"]).sort_index()
+
+    return (
+        df_prob,
+        df_inten,
+        df_prob.to_csv(data_path + "/probability_data_set.csv"),
+        df_inten.to_csv(data_path + "/intensity_data_set.csv"),
+    )
