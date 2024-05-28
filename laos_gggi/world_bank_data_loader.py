a330adeb56352f47420fe6b9c0ccc59efc28f91b
Camilo Saldarriaga <jcamsalda@gmail.com>
2024-05-28 09:18:26 -0600

    using pandas data reader to simplify the process, also update the constants file for that

--- laos_gggi/world_bank_data_loader.py
+++ laos_gggi/world_bank_data_loader.py
@@ -1,83 +1,60 @@
 import os
 from os.path import exists
-from urllib.request import urlretrieve
-from const_vars import GDP_PER_CAPITA_CONSTANT_URL, POPULATION_DENSITY_URL
-import pandas as pd
+from pandas_datareader import wb
+import numpy as np
+from const_vars import COUNTRIES_ISO, ISO_DICTIONARY
 
 
 def download_wb_data(output_path="../data"):
-    path_to_population_density = (
-        output_path + "/API_EN.POP.DNST_DS2_en_csv_v2_552619.zip"
-    )
-    path_to_gdp_per_capita_real = (
-        output_path + "/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_552878.zip"
-    )
     path_to_wb_data = output_path + "/world_bank.csv"
     if not exists(output_path):
         os.makedirs(output_path)
-        urlretrieve(POPULATION_DENSITY_URL, path_to_population_density)
-
-    if not exists(path_to_population_density):
-        urlretrieve(POPULATION_DENSITY_URL, path_to_population_density)
-
-    if not exists(path_to_gdp_per_capita_real):
-        urlretrieve(GDP_PER_CAPITA_CONSTANT_URL, path_to_gdp_per_capita_real)
-
-    if not exists(output_path + "/API_EN.POP.DNST_DS2_en_csv_v2_553809.csv"):
-        raise NotImplementedError("Unzip the population file")
-
-    if not exists(output_path + "/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_552878.csv"):
-        raise NotImplementedError("Unzip the GDP per Capita file")
-
-    if not exists(output_path + "/world_bank.csv"):
-        population_density = pd.read_csv(
-            "../data/API_EN.POP.DNST_DS2_en_csv_v2_553809.csv", skiprows=[0, 1, 2, 3]
+        # Importing data
+        wb_df = wb.download(
+            indicator=["EN.POP.DNST", "NY.GDP.PCAP.KD"],
+            country=COUNTRIES_ISO,
+            start="1900",
+            end=None,
         )
-        gdp_per_cap = pd.read_csv(
-            "../data/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_552878.csv", skiprows=[0, 1, 2, 3]
+        wb_df.reset_index(inplace=True)
+        # Adding country code
+        wb_df["country_code"] = np.NaN
+
+        for x in wb_df.index.values:
+            wb_df.loc[x, "country_code"] = ISO_DICTIONARY[wb_df.loc[x, "country"]]
+        # Formatting data
+        wb_df = wb_df[["country_code", "year", "NY.GDP.PCAP.KD", "EN.POP.DNST"]]
+        wb_df.rename(
+            columns={
+                "EN.POP.DNST": "population_density",
+                "NY.GDP.PCAP.KD": "gdp_per_cap",
+            },
+            inplace=True,
         )
+        wb_df = wb_df.set_index(["country_code", "year"]).sort_index()
 
-        population_density = (
-            population_density.drop(
-                ["Country Name", "Indicator Code", "Indicator Name"], axis=1
-            )
-            .melt(id_vars=["Country Code"])
-            .rename(
-                columns={
-                    "Country Code": "Country",
-                    "variable": "Year",
-                    "value": "Population_density",
-                }
-            )
-            .set_index(["Country", "Year"])
-            .sort_index()
+    else:
+        # Importing data
+        wb_df = wb.download(
+            indicator=["EN.POP.DNST", "NY.GDP.PCAP.KD"],
+            country=COUNTRIES_ISO,
+            start="1900",
+            end=None,
         )
-
-        gdp_per_cap = (
-            gdp_per_cap.drop(
-                ["Country Name", "Indicator Code", "Indicator Name"], axis=1
-            )
-            .melt(id_vars=["Country Code"])
-            .rename(
-                columns={
-                    "Country Code": "Country",
-                    "variable": "Year",
-                    "value": "gdp_per_cap_real",
-                }
-            )
-            .set_index(["Country", "Year"])
-            .sort_index()
+        wb_df.reset_index(inplace=True)
+        # Adding country code
+        wb_df["country_code"] = np.NaN
+        for x in wb_df.index.values:
+            wb_df.loc[x, "country_code"] = ISO_DICTIONARY[wb_df.loc[x, "country"]]
+        # Formatting data
+        wb_df = wb_df[["country_code", "year", "NY.GDP.PCAP.KD", "EN.POP.DNST"]]
+        wb_df.rename(
+            columns={
+                "EN.POP.DNST": "population_density",
+                "NY.GDP.PCAP.KD": "gdp_per_cap",
+            },
+            inplace=True,
         )
+        wb_df = wb_df.set_index(["country_code", "year"]).sort_index()
 
-        world_bank_file = pd.merge(
-            population_density,
-            gdp_per_cap,
-            left_index=True,
-            right_index=True,
-            how="inner",
-        )
-
-    if exists(path_to_wb_data):
-        world_bank_file = pd.read_csv(path_to_wb_data)
-
-    return (world_bank_file, world_bank_file.to_csv(path_to_wb_data))
+    return (wb_df, wb_df.to_csv(path_to_wb_data))
