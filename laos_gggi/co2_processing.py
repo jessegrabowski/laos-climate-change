b243e97b68165bec76b2f6b047f2bc6ed1896f55
andrewpwalters <47257455+andrewpwalters@users.noreply.github.com>
2024-05-31 13:21:06 +0200

    Fixes to #33
    Fixes to #33 requested by Jesse

--- laos_gggi/co2_processing.py
+++ laos_gggi/co2_processing.py
@@ -1,20 +1,27 @@
 import pandas as pd
 import os
+from os.path import exists
+from const_vars import CO2_URL, CO2_FILENAME
 
 
-def process_co2():
-    if not os.path.isfile("../data/co2.csv"):
-        url = "https://gml.noaa.gov/aftp/products/trends/co2/co2_mm_gl.csv"
-        df = pd.read_csv(url, skiprows=38)
-        df["day"] = 1
-        df["Date"] = pd.to_datetime(dict(year=df.year, month=df.month, day=df.day))
-        df = df.set_index("Date")
-        df.rename(columns={"average": "co2"}, inplace=True)
-        df = df[["co2"]]
-        df.to_csv("../data/co2.csv")
-        df_co2 = df
+def process_co2(data_path="../data"):
+    if not exists(data_path):
+        os.makedirs(data_path)
+
+    if not os.path.isfile(data_path + "/" + CO2_FILENAME):
+        url = CO2_URL
+        df_co2 = pd.read_csv(url, skiprows=38)
+        df_co2["Date"] = df_co2.apply(
+            lambda x: pd.to_datetime(f"{x.year}-{x.month}", format="%Y-%m", axis=1)
+        )
+        df_co2.set_index("Date", inplace=True)
+        df_co2.rename(columns={"average": "co2"}, inplace=True)
+        df_co2 = df_co2[["co2"]]
+        df_co2.to_csv(data_path + "/" + CO2_FILENAME)
 
     else:
-        df_co2 = pd.read_csv("../data/co2.csv", index_col=["Date"], parse_dates=True)
+        df_co2 = pd.read_csv(
+            data_path + "/" + CO2_FILENAME, index_col=["Date"], parse_dates=True
+        )
 
     return df_co2
--- laos_gggi/ocean_heat_processing.py
+++ laos_gggi/ocean_heat_processing.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import os
+from os.path import exists
+from const_vars import OCEAN_HEAT_FILENAME
 
 
-def process_ocean_heat():
-    if not os.path.isfile("../data/ncei_global_ocean_heat.csv"):
-        url = "https://www.ncei.noaa.gov/data/oceans/woa/DATA_ANALYSIS/3M_HEAT_CONTENT/DATA/basin/onemonth/ohc_levitus_climdash_monthly.csv"
-        # https://www.ncei.noaa.gov/access/global-ocean-heat-content/basin_heat_data_monthly.html
-        df = pd.read_csv(url, header=None)
-        df.rename(columns={0: "Date", 1: "Temp"}, inplace=True)
-        df.Date = pd.to_datetime(df.Date)
-        df = df.set_index("Date")
-        df.to_csv("../data/ncei_global_ocean_heat.csv")
-        df_ocean = df
+def process_ocean_heat(data_path="../data"):
+    if not exists(data_path):
+        os.makedirs(data_path)
+
+    if not os.path.isfile(data_path + "/" + OCEAN_HEAT_FILENAME):
+        url = "OCEAN_HEAT_URL"
+        df_ocean = pd.read_csv(url, header=None)
+        df_ocean.rename(columns={0: "Date", 1: "Temp"}, inplace=True)
+        df_ocean.Date = pd.to_datetime(df_ocean.Date)
+        df_ocean.set_index("Date", inplace=True)
+        df_ocean.to_csv(data_path + "/" + OCEAN_HEAT_FILENAME)
 
     else:
         df_ocean = pd.read_csv(
-            "../data/ncei_global_ocean_heat.csv", index_col=["Date"], parse_dates=True
+            data_path + "/" + OCEAN_HEAT_FILENAME, index_col=["Date"], parse_dates=True
         )
 
     return df_ocean
