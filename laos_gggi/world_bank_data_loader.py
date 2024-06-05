8fd0740f9dc14808a55b5363b4d64ba3b38e110c
Camilo Saldarriaga <jcamsalda@gmail.com>
2024-06-04 21:15:46 -0600

    eliminating the repeated if and using the avriable WB_INDICATORS

--- laos_gggi/world_bank_data_loader.py
+++ laos_gggi/world_bank_data_loader.py
@@ -9,24 +9,6 @@ def download_wb_data(folder_path="../data"):
     path_to_wb_data = folder_path + "/world_bank.csv"
     if not exists(folder_path):
         os.makedirs(folder_path)
-        # Importing data
-        wb_df = wb.download(
-            indicator=WB_INDICATORS,
-            country=COUNTRIES_ISO,
-            start="1900",
-            end=None,
-        )
-        wb_df.reset_index(inplace=True)
-        # Adding country code
-        wb_df["country_code"] = wb_df["country"].apply(ISO_DICTIONARY.get)
-        # Formatting data
-        wb_df = wb_df[["country_code", "year", "NY.GDP.PCAP.KD", "EN.POP.DNST"]]
-        wb_df.rename(
-            columns=WB_RENAME_DICT,
-            inplace=True,
-        )
-        wb_df = wb_df.set_index(["country_code", "year"]).sort_index()
-        wb_df.to_csv(path_to_wb_data)
 
     if not exists(path_to_wb_data):
         # Importing data
@@ -40,7 +22,7 @@ def download_wb_data(folder_path="../data"):
         # Adding country code
         wb_df["country_code"] = wb_df["country"].apply(ISO_DICTIONARY.get)
         # Formatting data
-        wb_df = wb_df[["country_code", "year", "NY.GDP.PCAP.KD", "EN.POP.DNST"]]
+        wb_df = wb_df[["country_code", "year"] + WB_INDICATORS]
         wb_df.rename(
             columns=WB_RENAME_DICT,
             inplace=True,
