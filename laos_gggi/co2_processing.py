654039daa6136bf849c3b8bebd94629cbc7f58ff
andrewpwalters <47257455+andrewpwalters@users.noreply.github.com>
2024-06-09 19:46:55 +0200

    Slight changes to climate data pull scripts
    Making Jesse's changes to the climate data pull scripts

--- laos_gggi/co2_processing.py
+++ laos_gggi/co2_processing.py
@@ -8,20 +8,19 @@ def process_co2(data_path="../data"):
     if not exists(data_path):
         os.makedirs(data_path)
 
-    if not os.path.isfile(data_path + "/" + CO2_FILENAME):
-        url = CO2_URL
-        df_co2 = pd.read_csv(url, skiprows=38)
+    if not os.path.isfile(os.path.join(data_path, CO2_FILENAME)):
+        df_co2 = pd.read_csv(CO2_URL, skiprows=38)
         df_co2["Date"] = df_co2.apply(
             lambda x: pd.to_datetime(f"{x.year}-{x.month}", format="%Y-%m", axis=1)
         )
         df_co2.set_index("Date", inplace=True)
         df_co2.rename(columns={"average": "co2"}, inplace=True)
         df_co2 = df_co2[["co2"]]
-        df_co2.to_csv(data_path + "/" + CO2_FILENAME)
+        df_co2.to_csv(os.path.join(data_path, CO2_FILENAME))
 
     else:
         df_co2 = pd.read_csv(
-            data_path + "/" + CO2_FILENAME, index_col=["Date"], parse_dates=True
+            os.path.join(data_path, CO2_FILENAME), index_col=["Date"], parse_dates=True
         )
 
     return df_co2
--- laos_gggi/ocean_heat_processing.py
+++ laos_gggi/ocean_heat_processing.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import os
 from os.path import exists
-from const_vars import OCEAN_HEAT_FILENAME
+from const_vars import OCEAN_HEAT_FILENAME, OCEAN_HEAT_URL
 
 
 def process_ocean_heat(data_path="../data"):
     if not exists(data_path):
         os.makedirs(data_path)
 
-    if not os.path.isfile(data_path + "/" + OCEAN_HEAT_FILENAME):
-        url = "OCEAN_HEAT_URL"
-        df_ocean = pd.read_csv(url, header=None)
+    if not os.path.isfile(os.path.join(data_path, OCEAN_HEAT_FILENAME)):
+        df_ocean = pd.read_csv(OCEAN_HEAT_URL, header=None)
         df_ocean.rename(columns={0: "Date", 1: "Temp"}, inplace=True)
         df_ocean.Date = pd.to_datetime(df_ocean.Date)
         df_ocean.set_index("Date", inplace=True)
-        df_ocean.to_csv(data_path + "/" + OCEAN_HEAT_FILENAME)
+        df_ocean.to_csv(os.path.join(data_path, OCEAN_HEAT_FILENAME))
 
     else:
         df_ocean = pd.read_csv(
-            data_path + "/" + OCEAN_HEAT_FILENAME, index_col=["Date"], parse_dates=True
+            os.path.join(data_path, OCEAN_HEAT_FILENAME),
+            index_col=["Date"],
+            parse_dates=True,
         )
 
     return df_ocean
