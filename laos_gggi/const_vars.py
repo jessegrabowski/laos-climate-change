f1e676706e0a322a21343e5d361418aadda6612a
Camilo Saldarriaga <jcamsalda@gmail.com>
2024-06-11 20:58:16 +0200

    Adding the filters and analyzing their impact on the data

--- laos_gggi/const_vars.py
+++ laos_gggi/const_vars.py
@@ -23,21 +23,38 @@ DISASTERS_FOUND = [  # noqa
 
 INTENSITY_COLS = [  # noqa
     "Country",
+    "Start_Year",
     "Region",
-    "Start Year",
-    "Total Deaths",
-    "No. Injured",
-    "No. Affected",
-    "No. Homeless",
-    "Total Affected",
-    "Reconstruction Costs ('000 US$)",
-    "Reconstruction Costs, Adjusted ('000 US$)",
-    "Insured Damage ('000 US$)",
-    "Insured Damage, Adjusted ('000 US$)",
-    "Total Damage ('000 US$)",
-    "Total Damage, Adjusted ('000 US$)",
+    "Deaths",
+    "Injured",
+    "Numb_Affected",
+    "Homeless",
+    "Total_Affected",
+    "Reconstruction_Costs",
+    "Reconstruction_Costs_Adjusted",
+    "Insured_Damage",
+    "Insured_Damage_Adjusted",
+    "Total_Damage",
+    "Total_Damage_Adjusted",
     "CPI",
 ]
+
+EM_DAT_COL_DICT = {  # noqa
+    "Start Year": "Start_Year",
+    "Total Deaths": "Deaths",
+    "No. Injured": "Injured",
+    "No. Affected": "Numb_Affected",
+    "No. Homeless": "Homeless",
+    "Total Affected": "Total_Affected",
+    "Reconstruction Costs ('000 US$)": "Reconstruction_Costs",
+    "Reconstruction Costs, Adjusted ('000 US$)": "Reconstruction_Costs_Adjusted",
+    "Insured Damage ('000 US$)": "Insured_Damage",
+    "Insured Damage, Adjusted ('000 US$)": "Insured_Damage_Adjusted",
+    "Total Damage ('000 US$)": "Total_Damage",
+    "Total Damage, Adjusted ('000 US$)": "Total_Damage_Adjusted",
+}
+
+
 WORLD_URL = "https://datacatalogfiles.worldbank.org/ddh-published/0038272/DR0046659/wb_countries_admin0_10m.zip?versionId=2024-05-14T14:58:01.5696428Z"  # noqa
 WORLD_FILENAME = "wb_countries_admin0_10m.zip"  # noqa
 LAOS_URL = "https://data.humdata.org/dataset/9eb6aff1-9e3f-43d3-99a6-f415fe4b4dff/resource/1d6edf99-5303-4b31-8909-dd70cda78443/download/lao_adm_ngd_20191112_shp.zip"  # noqa
--- laos_gggi/emdat_processing.py
+++ laos_gggi/emdat_processing.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import os
 from os.path import exists
-from laos_gggi.const_vars import INTENSITY_COLS, DISASTERS_FOUND, PROB_COLS  # noqa
+from laos_gggi.const_vars import (
+    INTENSITY_COLS,
+    DISASTERS_FOUND,
+    EM_DAT_COL_DICT,
+)  # noqa
 
 
 def process_emdat(data_path="../data"):
@@ -13,27 +17,66 @@ def process_emdat(data_path="../data"):
             "No EM-DAT data was found at `/data/emdat.xlsx`. Please make an account at https://public.emdat.be/, download the database, and place it in `/data/emdat.xlsx`"
         )
 
-    df = pd.read_excel(data_path + "/emdat.xlsx", sheet_name="EM-DAT Data")
+    df = pd.read_excel(data_path + "/emdat.xlsx", sheet_name="EM-DAT Data").rename(
+        columns=EM_DAT_COL_DICT
+    )
+    df_filtered = df.query("Total_Affected >1000 &  Deaths >100 & Start_Year > 1970")
+    df_raw = pd.read_excel(data_path + "/emdat.xlsx", sheet_name="EM-DAT Data").rename(
+        columns=EM_DAT_COL_DICT
+    )
+    df_raw_filtered = df_raw.query(
+        "Total_Affected >1000 &  Deaths >100 & Start_Year > 1970"
+    )
 
     df2 = df.copy()[INTENSITY_COLS]
+    df2_filtered = df.copy().query(
+        "Total_Affected >1000 &  Deaths >100 & Start_Year > 1970"
+    )[INTENSITY_COLS]
 
     df = (
         df.query("`Disaster Type` in @PROB_COLS")
-        .groupby(["Disaster Type", "Country", "Start Year", "Region"])
+        .groupby(["Disaster Type", "Country", "Start_Year", "Region"])
+        .size()
+        .unstack("Disaster Type")
+        .fillna(0)
+        .reset_index()
+    ).copy()
+    df_filtered = (
+        df_filtered.query("`Disaster Type` in @PROB_COLS")
+        .groupby(["Disaster Type", "Country", "Start_Year", "Region"])
         .size()
         .unstack("Disaster Type")
         .fillna(0)
         .reset_index()
     ).copy()
 
-    df[DISASTERS_FOUND] = df[DISASTERS_FOUND].astype(int).copy()
+    df_filtered[DISASTERS_FOUND] = df_filtered[DISASTERS_FOUND].astype(int).copy()
+    df_filtered[DISASTERS_FOUND] = df_filtered[DISASTERS_FOUND].astype(int).copy()
 
-    df_prob = df.copy().set_index(["Country", "Start Year"]).sort_index()
-    df_inten = df2.copy().set_index(["Country", "Start Year"]).sort_index()
+    df_prob = (
+        df.copy()
+        .set_index(["Country", "Start_Year"])
+        .sort_index()
+        .rename(columns=EM_DAT_COL_DICT)
+    )
+    df_prob_filtered = (
+        df_filtered.copy()
+        .set_index(["Country", "Start_Year"])
+        .sort_index()
+        .rename(columns=EM_DAT_COL_DICT)
+    )
+    df_inten = df2.copy().set_index(["Country", "Start_Year"]).sort_index()
+    df_inten_filtered = (
+        df2_filtered.copy().set_index(["Country", "Start_Year"]).sort_index()
+    )
 
     return (
         df_prob,
         df_inten,
+        df_raw,
+        df_prob_filtered,
+        df_inten_filtered,
+        df_raw_filtered,
         df_prob.to_csv(data_path + "/probability_data_set.csv"),
         df_inten.to_csv(data_path + "/intensity_data_set.csv"),
     )
