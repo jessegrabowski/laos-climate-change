3fb1c278be243fc864b0c944dd88455b704fbe86
Camilo Saldarriaga <jcamsalda@gmail.com>
2024-06-24 13:59:13 +0200

    fixing functions

--- laos_gggi/co2_processing.py
+++ laos_gggi/co2_processing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from os.path import exists
-from const_vars import CO2_URL, CO2_FILENAME
+from laos_gggi.const_vars import CO2_URL, CO2_FILENAME
 
 
 def process_co2(data_path="../data"):
@@ -10,9 +10,8 @@ def process_co2(data_path="../data"):
 
     if not os.path.isfile(os.path.join(data_path, CO2_FILENAME)):
         df_co2 = pd.read_csv(CO2_URL, skiprows=38)
-        df_co2["Date"] = pd.to_datetime(
-            df_co2["year"].astype(str) + "-" + df_co2["month"].astype(str),
-            format="%Y-%m",
+        df_co2["Date"] = df_co2.apply(
+            lambda x: pd.to_datetime(f"{x.year}-{x.month}", format="%Y-%m", axis=1)
         )
         df_co2.set_index("Date", inplace=True)
         df_co2.rename(columns={"average": "co2"}, inplace=True)
