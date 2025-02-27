186a425e8ab16fa82a2cfa5b188b09754d4187c8
jessegrabowski <jessegrabowski@gmail.com>
2025-02-27 23:34:31 +0800

    Revised events model

--- laos_gggi/data_functions/combine_data.py
+++ laos_gggi/data_functions/combine_data.py
@@ -20,7 +20,7 @@ def load_all_data():
     # 2. EM-DAT data representing the event damages (index: Year, ISO3)
     merged_dict["emdat_damage"] = emdat["df_inten_filtered_adjusted"]
     merged_dict["emdat_damage_hydro"] = emdat["df_inten_filtered_adjusted_hydro"]
-    merged_dict["emdat_damage_clim"] = emdat["df_inten_filtered_adjusted"]
+    merged_dict["emdat_damage_clim"] = emdat["df_inten_filtered_adjusted_clim"]
 
     emdat["df_inten_filtered_adjusted_hydro"] = (
         emdat["df_inten_filtered_adjusted_hydro"]
