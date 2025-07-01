a03e3e026cd8abc933b8e72169bc073f320b7e59
CamSalda <131469693+CamSalda@users.noreply.github.com>
2025-07-01 12:12:54 +0200

    asean_demo (#122)
    * asean_demo
    
    * fixing the html in _load_disaster_point_data
    
    * demo clean version
    
    * adding demo pieces
    
    * laos_gggi/data_functions/disaster_point_data.py

--- laos_gggi/data_functions/ocean_heat_processing.py
+++ laos_gggi/data_functions/ocean_heat_processing.py
@@ -5,13 +5,15 @@ from os.path import exists
 from laos_gggi.const_vars import OCEAN_HEAT_FILENAME, OCEAN_HEAT_URL
 
 
-def load_ocean_heat_data(data_path=None):
+def load_ocean_heat_data(data_path=None, force_reload: bool = False):
     if data_path is None:
         data_path = here("data")
     if not exists(data_path):
         os.makedirs(data_path)
 
-    if not os.path.isfile(os.path.join(data_path, OCEAN_HEAT_FILENAME)):
+    if (
+        not os.path.isfile(os.path.join(data_path, OCEAN_HEAT_FILENAME))
+    ) or force_reload:
         df_ocean = pd.read_csv(OCEAN_HEAT_URL, header=0, names=["Date", "Temp"])
         df_ocean.Date = pd.to_datetime(df_ocean.Date, format="%Y-%m")
         df_ocean.set_index("Date", inplace=True)
@@ -19,7 +21,6 @@ def load_ocean_heat_data(data_path=None):
         df_ocean.reset_index(inplace=True)
         df_ocean["Date"] = df_ocean["Date"] - pd.offsets.YearBegin()
         df_ocean.set_index("Date", inplace=True)
-        df_ocean = df_ocean.iloc[1:-1]
         df_ocean = df_ocean + 152
         df_ocean.to_csv(os.path.join(data_path, OCEAN_HEAT_FILENAME))
 
