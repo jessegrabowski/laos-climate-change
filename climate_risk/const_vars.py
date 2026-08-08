c0023c7caaf69161db527d3ded5992cf755cebee
jessegrabowski <jessegrabowski@gmail.com>
2026-08-08 12:47:55 -0500

    Delete the constants nothing imports

--- climate_risk/const_vars.py
+++ climate_risk/const_vars.py
@@ -13,13 +13,6 @@ PROB_COLS = [
     "Subregion",
 ]
 
-DISASTERS_FOUND = [
-    "Drought",
-    "Extreme temperature",
-    "Flood",
-    "Storm",
-    "Wildfire",
-]
 
 INTENSITY_COLS = [
     "Country",
@@ -52,8 +45,6 @@ EM_DAT_COL_DICT = {
 }
 
 
-MODEL_DF_FILENAME = "model_df.csv"
-DAMAGE_DF_FILENAME = "damage_df.csv"
 LAOS_LOCATION_DICTIONARY = {
     "1971-0048-LAO": {"Latitude": 17.9757, "Longitude": 102.6331},
     "2000-0583-LAO": {"Latitude": 19.0, "Longitude": 102.0},
@@ -61,15 +52,6 @@ LAOS_LOCATION_DICTIONARY = {
     "2013-0417-LAO": {"Latitude": 16.5, "Longitude": 106.0},
     "2015-0324-LAO": {"Latitude": 19.0, "Longitude": 104.0},
     "2016-0316-LAO": {"Latitude": 19.9, "Longitude": 102.1},
-}
-
-
-POPULATION_DENSITY_URL = "https://api.worldbank.org/v2/en/indicator/EN.POP.DNST?downloadformat=csv"
-GDP_PER_CAPITA_CONSTANT_URL = "https://api.worldbank.org/v2/en/indicator/NY.GDP.PCAP.KD?downloadformat=csv"
-
-VARIABLES_DICTIONARY = {
-    "gdp_per_cap_real": "GDP per capita (constant 2015 US$)",
-    "population_density": "Population density (people per sq. km of land area)",
 }
 
 
